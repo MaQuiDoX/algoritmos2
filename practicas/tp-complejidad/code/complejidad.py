3775b7ac1df1df74e8d39cc3e325ba6d58ca43b8
MaQuiDoX <matuquesada999@gmail.com>
2023-03-21 09:03:55 -0300

    Implementada una parte de orderHalfMinors(), quedan solucionar algunos casos y consultar dudas

--- practicas/tp-complejidad/code/complejidad.py
+++ practicas/tp-complejidad/code/complejidad.py
@@ -1,4 +1,5 @@
 from linkedlist import *
+import math
 
 def ContieneSuma(A, n):
   lenA = lengthList(A)
@@ -16,3 +17,56 @@ def ContieneSuma(A, n):
     currentNode = currentNode2
     lenB = lenB - 1
   return False
+
+def orderHalfMinors(A):
+  lenA = lengthList(A)
+  midPosition = math.trunc(lenA/2)-1
+  midValue = accessList(A, midPosition)
+  minorsHalf = 0
+  minorsList = 0
+  currentNode1 = A.head
+
+  #Calculo cantidad de menores al valor del medio que hay en la mitad
+  for i in range(midPosition):
+    if currentNode1.value < midValue:
+      minorsHalf += 1
+    currentNode1 = currentNode1.nextNode
+  currentNode1 = A.head
+
+  #Calculo cantidad de menores al valor del medio en toda la lista
+  for j in range(lenA):
+    if currentNode1.value < midValue:
+      minorsList += 1
+    currentNode1 = currentNode1.nextNode
+  currentNode1 = A.head
+
+  '''
+  Solucionar (Puede que hasta la mitad haya mas menores al valor del medio que en toda la lista, condicional, 2 casos)
+  '''
+  while math.trunc(minorsList/2) != minorsHalf:
+        for k in range(midPosition, lenA):
+          if currentNode1.value < midValue:
+            '''
+            move(A, currentNode1, 0)
+            Duda sobre que sucede si el medio se mueve de lugar
+            '''
+            minorsHalf += 1
+            break
+          currentNode1 = currentNode1.nextNode
+  return A
+
+A = LinkedList()
+addList(A,1)
+addList(A,1)
+addList(A,2)
+addList(A,8)
+addList(A,4)
+addList(A,7)
+addList(A,1)
+addList(A,6)
+
+printlinkedlist(A)
+
+orderHalfMinors(A)
+
+printlinkedlist(A)
